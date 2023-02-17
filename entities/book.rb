394c2adb51340aa8672d54134a66e40958a2963b
Amen TETTEH <tkamen@outlook.com>
2023-02-17 14:21:39 +0000

    Update book.rb file

--- entities/book.rb
+++ entities/book.rb
@@ -13,4 +13,16 @@ class Book
     rental = Rental.new(date, self, person)
     @rentals.push(rental)
   end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'title' => @title,
+      'author' => @author
+    }.to_json(*args)
+  end
+
+  def self.json_create(object)
+    new(object['title'], object['author'])
+  end
 end
