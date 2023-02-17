5c46cf5c13e7aaad583f802dc79553ecc4881769
hopeforkoami <hopeforkoami@gmail.com>
2023-02-17 14:32:31 +0000

    Update student import and export in json

--- entities/student.rb
+++ entities/student.rb
@@ -2,9 +2,11 @@ require_relative './person'
 
 class Student < Person
   attr_reader :classroom
-
-  def initialize(_classroom, age, name: 'Unknown', parent_permission: true)
-    super(age, name: name, parent_permission: parent_permission)
+  attr_accessor :id
+ 
+  def initialize(classroom, age, name = 'unknown', parent_permission = true, id = Random.rand(1..1000))
+    super(age, name, parent_permission, id)
+    @classroom = classroom
   end
 
   def play_hooky
@@ -14,5 +16,20 @@ class Student < Person
   def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
+  end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'id' => @id,
+      'classroom' => @classroom,
+      'age' => @age,
+      'name' => @name,
+      'parent_permission' => @parent_permission
+    }.to_json(*args)
+  end
+
+  def self.json_create(object)
+    new(object['classroom'], object['age'], object['name'], object['parent_permission'], object['id'])
   end
 end
