2941e8e3f2612c3c2db0aa4e8fb98c7b7b30e188
hopeforkoami <hopeforkoami@gmail.com>
2023-02-18 16:50:33 +0000

    Update capitalize_spec.rb

--- spec/capitalize_spec.rb
+++ spec/capitalize_spec.rb
@@ -4,7 +4,7 @@ require_relative '../entities/person'
 describe CapitalizeDecorator do
   before :each do
     @person = Person.new 2333, 12, 'serena tetteh', 'n'
-    @capitalize_decorator = CapitalizeDecorator.new @person
+    @capitalize_decorator = CapitalizeDecorator.new @person.name
   end
   it 'Given one parameter, a BaseDecorator object is returns' do
     expect(@capitalize_decorator).to be_an_instance_of CapitalizeDecorator
