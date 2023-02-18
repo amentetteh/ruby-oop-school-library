d9fb1117b39e29c8bb81e4eee362f631e66b7e4e
hopeforkoami <hopeforkoami@gmail.com>
2023-02-18 17:08:35 +0000

    Update decorators_spec.rb

--- spec/decorators_spec.rb
+++ spec/decorators_spec.rb
@@ -5,14 +5,14 @@ describe Nameable do
   end
 
   context 'When testing a Nameable class' do
-    it 'should be an instance of Nameable object' do
+    it 'Given no parameters , should returns a Person object' do
       expect(@nameable).to be_an_instance_of Nameable
     end
   end
 
   context 'When using correct_name method' do
-    it 'should raise an error' do
-      expect { @nameable.correct_name }.to raise_error('NotImplementedError')
+    it 'correct_name method should raise Not Implemented error ' do
+      expect { @nameable.correct_name }.to raise_error(NotImplementedError)
     end
   end
 end
